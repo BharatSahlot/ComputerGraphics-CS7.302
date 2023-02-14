3c4246add0a3ac770706f4d6815426b59c065fb1
BharatSahlot <bharatsahlot18@gmail.com>
2023-02-15 01:07:24 +0530

    fix car origin, spawn car at start

--- Game/Worlds/GameWorld.cpp
+++ Game/Worlds/GameWorld.cpp
@@ -7,6 +7,10 @@ GameWorld::GameWorld(std::shared_ptr<Window> window, Game* game) : World(window)
         "Assets/race_track.fbx"
     });
 
+    GetResourceManager().AddInResourceQueue("car", ResourceLoadData<Model> {
+        "Car/sportste.fbx"
+    });
+
     camera = Instantiate<Camera>("camera", glm::vec3(0, 15, -50), glm::vec3(0, 0, 0));
     camera->clearColor = glm::vec3(21.f, 154.f, 198.f) / 255.f;
     camera->canMove = true;
@@ -18,4 +22,8 @@ void GameWorld::Start()
     window->SetCamera(camera);
 
     Instantiate<Object>("raceTrack", "RaceTrack");
+
+    Object* obj = GetObjectByName<Object>("Start");
+    auto car = Instantiate<Object>("playerCar", "car");
+    car->transform->SetLocalPosition(obj->transform->GetWorldPosition());
 }
